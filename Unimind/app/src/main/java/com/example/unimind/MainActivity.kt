4c1c2480e9ca13eb9be1047f1a0a7abb8e643b02
RafaellysSilva <cc24153@g.unicamp.br>
2025-03-23 21:35:12 -0300

    configurações "pronta"
    visualmente

--- Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
+++ Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
@@ -7,12 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+/*import androidx.compose.foundation.layout.BoxScopeInstance.align
+import androidx.compose.foundation.layout.ColumnScopeInstance.align
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align*/
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -23,10 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
@@ -71,6 +83,7 @@ fun AppNavigation() {
         composable("telaFlashCardsArea") { FlashcardsArea(navController) }
         composable("telaFlashCardsPergunta") { FlashcardsPergunta(navController) }
         composable("telaFlashcardsResposta") { FlashcardsResposta(navController) }
+        composable("telaConfiguracoes") { telaConfiguracoes(navController) }
     }
 }
 
@@ -343,12 +356,13 @@ fun Cadastro(navController: NavController) {
                         .fillMaxWidth(),
                     contentAlignment = Alignment.TopCenter
                 ) {
-                    Column {
+                    Column(/*modifier = Modifier.background(color = Color.Blue)*/) {
                         Text(
                             text = "Cadastro",
                             fontFamily = inter,
                             fontWeight = FontWeight.Bold,
                             fontSize = 50.sp,
+                            //background = null,
                             color = Color(0xFF741C28),
                             modifier = Modifier.padding(0.dp, 40.dp, 0.dp, 0.dp)
                         )
@@ -894,6 +908,358 @@ fun FlashcardsResposta(navController: NavController) {
                     }
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@Composable
+fun telaConfiguracoes(navController: NavController) {
+    val inter = FontFamily(
+        Font(R.font.inter)
+    )
+    UnimindTheme {
+        //É o cabeçário da página
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Nude
+        ) {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top,
+                //horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    //mexer no alinhamento do texto e da imagem
+                    Modifier
+                        .height(95.dp)
+                        .background(Vinho)
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+
+
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically, // Alinha os itens no centro verticalmente
+                        //horizontalArrangement = Arrangement.Center, // Centraliza na horizontal
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 15.dp, 0.dp, 0.dp)
+                    ) {
+                        Image(
+                            painterResource(id = R.drawable.bichinho),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(start = 5.dp, end = 170.dp)
+                                .width(50.dp)
+                                .height(50.dp)
+                        )
+                        Text(
+                            text = "Configurações",
+                            fontFamily = inter,
+                            color = White,
+                            fontSize = 20.sp,
+                            /*modifier = Modifier
+                                .padding(start = 10.dp)*/
+                        )
+                        Image(
+                            painterResource(id = R.drawable.engranagem),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .height(30.dp)
+                                .width(30.dp)
+                                .padding(start = 10.dp)
+                        )
+                    }
+                    Box(
+                        Modifier
+                            .padding(top = 40.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Image(
+                            painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clip(shape = CircleShape)
+                                .align(BottomCenter)
+                                .background(Blue)
+                        )
+                    }
+                }
+            //corpo da tela
+                Spacer(Modifier.height(70.dp))
+            Box{
+                Column{
+                    Text(text = "Perfil",
+                    fontSize = 23.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFFf3d1c2))
+                        .padding(top = 5.dp, bottom = 5.dp, start = 10.dp)
+                    )
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        var user by remember { mutableStateOf("") }
+                        Text("Usuário:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp)
+                        )
+                        OutlinedTextField(
+                            value = user,
+                            onValueChange = {user = it},
+                            modifier = Modifier
+                                .width(280.dp)
+                                .height(10.dp)
+                                .padding(start = 20.dp, top = 10.dp)
+                        )
+                    }
+
+
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        var nome by remember { mutableStateOf("") }
+                        Text("Nome:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 17.dp)
+                        )
+                        OutlinedTextField(
+                            value = nome,
+                            onValueChange = {nome = it},
+                            modifier = Modifier
+                                .width(280.dp)
+                                .height(10.dp)
+                                .padding(start = 20.dp, top = 10.dp)
+                        )
+                    }
+
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        var email by remember { mutableStateOf("") }
+                        Text("E-mail:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 12.dp)
+                        )
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = {email = it},
+                            modifier = Modifier
+                                .width(280.dp)
+                                .height(10.dp)
+                                .padding(start = 20.dp, top = 10.dp)
+                        )
+                    }
+
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        var senha by remember { mutableStateOf("") }
+                        Text("Senha:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 14.dp)
+                        )
+                        OutlinedTextField(
+                            value = senha,
+                            onValueChange = {senha = it},
+                            modifier = Modifier
+                                .width(280.dp)
+                                .height(10.dp)
+                                .padding(start = 20.dp, top = 10.dp)
+                        )
+                    }
+
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        var nivel by remember { mutableStateOf("") }
+                        Text("Nível:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 24.dp)
+                        )
+                        OutlinedTextField(
+                            value = nivel,
+                            readOnly = true,
+                            onValueChange = {nivel = it},
+                            modifier = Modifier
+                                .width(280.dp)
+                                .height(10.dp)
+                                .padding(start = 20.dp, top = 10.dp)
+                        )
+                    }
+                    Spacer(Modifier.height(20.dp))
+                    Text(text = "Preferências",
+                        fontSize = 23.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFFf3d1c2))
+                            .padding(start = 10.dp, top = 5.dp, bottom = 5.dp)
+                    )
+                    Spacer(Modifier.height(15.dp))
+
+                    Row{
+                        Text("Dificuldade:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 14.dp, top = 3.dp)
+                        )
+                        var expanded by remember { mutableStateOf(false) }
+                        var selecionada by remember { mutableStateOf("") }
+                        val opcoes = listOf("Opção 1", "Opção 2", "Opção 3")
+                        ExposedDropdownMenuBox(
+                            expanded = expanded,
+                            onExpandedChange = { expanded = !expanded }
+                        ) {
+                            OutlinedTextField(
+                                value = selecionada,
+                                onValueChange = {},
+                                readOnly = true,
+                                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                                modifier = Modifier
+                                    .menuAnchor() // Necessário para alinhar o menu corretamente
+                                    .height(30.dp)
+                                    .width(250.dp)
+                            )
+
+                            ExposedDropdownMenu(
+                                expanded = expanded,
+                                onDismissRequest = { expanded = false }
+                                //Modifier.height(50.dp)
+                            ) {
+                                opcoes.forEach { opcao ->
+                                    DropdownMenuItem(
+                                        text = { Text(opcao) },
+                                        onClick = {
+                                            selecionada = opcao
+                                            expanded = false
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Spacer(Modifier.height(15.dp))
+                    Row{
+                        Text("Facilidade:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 23.dp, top = 3.dp)
+                        )
+                        var expanded by remember { mutableStateOf(false) }
+                        var selecionada by remember { mutableStateOf("") }
+                        val opcoes = listOf("Opção 1", "Opção 2", "Opção 3")
+                        ExposedDropdownMenuBox(
+                            expanded = expanded,
+                            onExpandedChange = { expanded = !expanded }
+                        ) {
+                            OutlinedTextField(
+                                value = selecionada,
+                                onValueChange = {},
+                                readOnly = true,
+                                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                                modifier = Modifier
+                                    .menuAnchor() // Necessário para alinhar o menu corretamente
+                                    .height(30.dp)
+                                    .width(250.dp)
+                            )
+
+                            ExposedDropdownMenu(
+                                expanded = expanded,
+                                onDismissRequest = { expanded = false }
+                                //Modifier.height(50.dp)
+                            ) {
+                                opcoes.forEach { opcao ->
+                                    DropdownMenuItem(
+                                        text = { Text(opcao) },
+                                        onClick = {
+                                            selecionada = opcao
+                                            expanded = false
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    Spacer(Modifier.height(8.dp))
+                    Row{
+                        var minutos by remember { mutableStateOf("") }
+                        Text("Tempo em \nresolução\nde listas:", fontSize = 15.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 14.dp, top = 3.dp)
+                        )
+                        OutlinedTextField(
+                            value = minutos,
+                            onValueChange = {minutos = it},
+                            modifier = Modifier
+                                .height(30.dp)
+                                .width(281.dp)
+                                .padding(top = 40.dp, start = 32.dp)
+                        )
+                    }
+
+                    Spacer(Modifier.height(20.dp))
+                    Text(text = "Outros",
+                        fontSize = 23.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFFf3d1c2))
+                            .padding(start = 10.dp, top = 5.dp, bottom = 5.dp)
+                    )
+
+                    Spacer(Modifier.height(20.dp))
+
+                    Row{
+                        Text("Excluir conta:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 23.dp, top = 8.dp)
+                        )
+                        OutlinedButton (
+                            onClick = { print("Clicou no excluir") },
+                            //border = BorderStroke(2.dp, Vinho),
+                            modifier = Modifier
+                                .width(200.dp)
+                                .height(40.dp)
+
+                        ) {
+                            Image(
+                                painterResource(id = R.drawable.lixeira),
+                                contentDescription = null
+                            )
+                        }
+                    }
+
+                    Spacer(Modifier.height(10.dp))
+
+                    Row{
+                        Text("Nosso contato:", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(start = 10.dp, end = 23.dp, top = 8.dp)
+                        )
+                        Text(text = "email@example.com", fontSize = 20.sp, modifier = Modifier
+                            .padding(top = 7.dp))
+
+                    }
+
+                    Spacer(Modifier.height(15.dp))
+
+                    Row(
+                        Modifier.padding(start = 120.dp)
+                    ){
+                        OutlinedButton(
+                            onClick = {}
+
+                        ) {
+                            Text(text = "Salvar alterações", color = Black)
+                        }
+                    }
+
+
+                }
+            }
+            }
+
         }
     }
 }

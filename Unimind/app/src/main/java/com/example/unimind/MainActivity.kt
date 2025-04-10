eb129e91d3d9976e5de55d195921c4932568be2e
Mariana <133607739+mariwgh@users.noreply.github.com>
2025-04-10 10:57:14 -0300

    headerrr

--- Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
+++ Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 /*import androidx.compose.foundation.layout.BoxScopeInstance.align
@@ -133,7 +134,7 @@ fun Header(nomePagina: String){
 
 //função para chamar (tem várias telas que tem a mesma parte como o footer, fica mais facil assim)
 @Composable
-fun Footer() {
+fun Footer(navController: NavController) {
     Column(
         Modifier
             .fillMaxSize()
@@ -153,6 +154,10 @@ fun Footer() {
                 Image(
                     painterResource(id = R.drawable.flashcards),
                     contentDescription = null,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("telaFlashcardsArea")
+                        }
                 )
 
                 Spacer(
@@ -193,6 +198,10 @@ fun Footer() {
                 Image(
                     painterResource(id = R.drawable.bichinho),
                     contentDescription = null,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("telaInicial")
+                        }
                 )
             }
         }
@@ -1091,13 +1100,11 @@ fun Inicial(navController: NavController){
                             Text("Estatísticas", color = White, modifier = Modifier.padding(start = 27.dp))
 
                         }
-
                     }
-
                 }
             }
-
         }
+        Footer(rememberNavController())
     }
 }
 
@@ -1202,7 +1209,7 @@ fun FlashcardsArea(navController: NavController) {
                 }
             }
 
-            Footer()
+            Footer(rememberNavController())
         }
     }
 }
@@ -1422,5 +1429,5 @@ fun FlashcardsResposta(navController: NavController) {
 @Preview(showSystemUi = true)
 @Composable
 fun AppPreview(){
-    Bloqueio(rememberNavController())
+    Inicial(rememberNavController())
 }

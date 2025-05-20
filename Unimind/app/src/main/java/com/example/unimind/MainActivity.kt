fe04dd7dd431cbaacc70917840b7956b53f6f023
RafaellysSilva <162441877+RafaellysSilva@users.noreply.github.com>
2025-05-20 16:18:28 -0300

    ..

--- Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
+++ Unimind/app/src/main/java/com/example/unimind/MainActivity.kt
@@ -643,7 +643,7 @@ fun Entrar(navController: NavController, viewModel: UsuarioViewModel = viewModel
 
                         OutlinedButton (
                             onClick = {
-                                if (usuarioIdBusca.isNotEmpty()) {
+                                if (user.isNotEmpty() && password.isNotEmpty()) {
                                     viewModel.buscarUsuario(user.toString(), password.toString())
                                 }
                             },

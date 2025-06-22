0f6a9a375239d52d7c06ed214a03d919ff0606ae
JoaoCussolim <joaovccussolim@gmail.com>
2025-06-22 18:57:34 -0300

    dockerignore

--- obj/Debug/net9.0/UnimindAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/UnimindAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnimindAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21638c7d104e17be54345f437a4d4a155b5a5e7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d9c372e87576977ac8f02b3e921ac1bb2a45a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnimindAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnimindAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
